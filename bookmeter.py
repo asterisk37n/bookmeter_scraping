ed3ea2e01f81c52ba9601e4e3607cbeb44add20a
root <root@ip-172-31-21-108.us-west-2.compute.internal>
2017-04-19 14:46:43 +0000

    go with new bookmeter.com

--- bookmeter.py
+++ bookmeter.py
@@ -6,25 +6,22 @@ import lxml.html
 import datetime
 import requests
 import time
+import re
+import grequests
+from bs4 import BeautifulSoup
 
 class BookmeterScraping():
     """
-    Methods:
-        get_isbns_to_read(debug=False): set and return self.isbns_to_read
-        _get_isbn_from_URL(url)
     """
     def __init__(self, bookmeter_ID):
         self.bookmeter_ID = bookmeter_ID if isinstance(bookmeter_ID, str) else str(bookmeter_ID)
         self.isbns_to_read = []
         self.last_page_number = -1
-    def get_isbns_to_read(self, debug=False):
+        self.endpoint = 'https://bookmeter.com'
+    def get_isbns_to_read(self):
         """
             debug==True is for offline mode.
         """
-        if debug == True:
-            self.isbns_to_read = ['479422155X', '4622076519', '4062579359', '4062183765', '4822249417', '4797338121',
-                             '439313544X', '4166601741', '4569697135', '486182334X', '483345002X', '4062883600']
-            return self.isbns_to_read
         page=1
         while True:
             next_isbns = self.get_isbns_in_page(page)
@@ -34,38 +31,36 @@ class BookmeterScraping():
                 time.sleep(.5)
             else:
                 break
-        print(self.isbns_to_read)
         return self.isbns_to_read
 
     def get_isbns_in_page(self, page):
         url = 'https://bookmeter.com/users/{id}/books/wish?page={page}'.format(**{'id':self.bookmeter_ID, 'page':page})
-        isbns_in_page = self._get_isbn_from_URL(url)
-        return isbns_in_page        
+        r = requests.get(url)
+        soup = BeautifulSoup(r.content, 'html.parser')
+        divs = soup.findAll('div', class_='thumbnail__cover')
+        urls = [self.endpoint + div.find('a')['href'] for div in divs]
+        rs = (grequests.get(u) for u in urls)
+        responses = grequests.map(rs)
+        sources = (r.content for r in responses)
+        hrefs = (BeautifulSoup(s, 'html.parser').find('a', target='_blank')['href'] for s in sources)
+        isbns = ((lambda x: re.findall(r'\d{9}[\dX]',x)[0])(href) for href in hrefs)
+        if page == 1:
+            self._get_last_page_number(r.content)
+        return isbns
 
-
-    def _get_isbn_from_URL(self, url):
-        page = requests.get(url)
-        tree = lxml.html.fromstring(page.content)
-        href_list = tree.xpath('//div[@class="book_box_book_title"]/a/@href')
-        isbns_in_page = [href[3:] for href in href_list if href.startswith('/b')]
-        self._get_last_page_number(tree)
-        return isbns_in_page
-
-    def _get_last_page_number(self, tree):        
-        page_navi_hedge = tree.xpath('//span[@class="page_navi_hedge"]/a/@href') # start or last link
-        page_navis = tree.xpath('//div[@class="page_navis"]')
-        if page_navis: # if page has links to another page
-            href = page_navis[0][-1][0].get('href')
-            if not href.endswith('booklistpre') and self.last_page_number<0:
-                self.last_page_number = int(href.split('=')[-1])
-            else:
-                pass
-            print(self.last_page_number)
-            return self.last_page_number
+    def _get_last_page_number(self, wishpage_source): 
+        soup = BeautifulSoup(wishpage_source, 'html.parser')
+        a = soup.findAll('a', class_='bm-pagination__link')
+        if a: # if page has links to another page
+            self.last_page_number = re.findall(r'\d+$', a[-1]['href'])[-1]
         else:
             self.last_page_number = 1
+        return self.last_page_number
 
 if __name__ == '__main__':
     bs = BookmeterScraping('107634')
-    bs.get_isbns_in_page(1)
-
+    isbns = bs.get_isbns_in_page(1)
+    print(list(isbns))
+    bs = BookmeterScraping('104933')
+    isbns = bs.get_isbns_in_page(1)
+    print(list(isbns))
